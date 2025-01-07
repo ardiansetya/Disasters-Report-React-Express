a002dfdb228bd04b2c002dc671fff09b8f792a2c
ardiansetya <ardiansetya129@gmail.com>
2025-01-07 20:44:52 +0700

    fix/corspt2

--- be/src/index.js
+++ be/src/index.js
@@ -13,15 +13,15 @@ const app = express();
 app.use(helmet());
 app.use(express.json());
 app.use(cookieParser())
-const corsOptions = {
-    origin: [
-        "http://localhost:4173", // Untuk pengembangan lokal
-        "https://disasters-report-react-express.vercel.app" // URL frontend yang di-deploy
-    ],
-    methods: ["GET", "POST", "PUT", "DELETE"], // Metode HTTP yang diizinkan
-    credentials: true // Jika Anda menggunakan cookies atau header Authorization
-};
-app.use(cors(corsOptions));
+// const corsOptions = {
+//     origin: [
+//         "http://localhost:4173", // Untuk pengembangan lokal
+//         "https://disasters-report-react-express.vercel.app" // URL frontend yang di-deploy
+//     ],
+//     methods: ["GET", "POST", "PUT", "DELETE"], // Metode HTTP yang diizinkan
+//     credentials: true // Jika Anda menggunakan cookies atau header Authorization
+// };
+app.use(cors());
 
 // Endpoint root
 app.get('/', (req, res) => {
